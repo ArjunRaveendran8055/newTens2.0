10cc11785d9c41ab955d7d4627d68bcb3bdac8d6
Fayaz900 <fayazazeez2@gmail.com>
2024-03-03 15:47:14 +0530

    user card data seperated to other

---
+++ client/src/components/features/user/statisticsCard/AdminStatisticsCard.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import StatisticsCard from '../../../../widgets/cards/StatisticsCard';
+import { Typography } from '@material-tailwind/react';
+import {
+  BanknotesIcon,
+  UserPlusIcon,
+  UsersIcon,
+  ChartBarIcon,
+} from "@heroicons/react/24/solid";
+
+function AdminStatisticsCard() {
+
+    const adminStatisticsCardsData = [
+        {
+          color: "gray",
+          icon: BanknotesIcon,
+          title: "Today's Money",
+          value: "$53k",
+          footer: {
+            color: "text-green-500",
+            value: "+55%",
+            label: "than last week",
+          },
+        },
+      
+        {
+          color: "gray",
+          icon: UserPlusIcon,
+          title: "APPROVE USERS",
+          value: "Pending list",
+          footer: {
+            color: "text-red-500",
+            value: "7",
+            label: "users waiting for Attention",
+          },
+        },
+        {
+          color: "gray",
+          icon: ChartBarIcon,
+          title: "Sales",
+          value: "$103,430",
+          footer: {
+            color: "text-green-500",
+            value: "+5%",
+            label: "than yesterday",
+          },
+        },
+        {
+          color: "gray",
+          icon: UsersIcon,
+          title: "STUDENTS",
+          value: "CRM",
+          footer: {
+            color: "text-green-500",
+            value: "+3%",
+            label: "usage than last month",
+          },
+        },
+      ];
+
+  return (
+    <>
+        {adminStatisticsCardsData.map(({ icon, title, footer, ...rest }) => (
+            <StatisticsCard
+            key={title}
+            {...rest}
+            title={title}
+            icon={React.createElement(icon, {
+              className: "w-6 h-6 text-white",
+            })}
+            footer={
+              <Typography className="font-normal text-blue-gray-600">
+                <strong className={footer.color}>{footer.value}</strong>
+                &nbsp;{footer.label}
+              </Typography>
+            }
+          />
+        ))
+      }
+    </>
+  )
+}
+
+export default AdminStatisticsCard
--- client/src/pages/dashboard/home.jsx
+++ client/src/pages/dashboard/home.jsx
@@ -21,64 +21,10 @@ import projectsTableData from "../../data/projectsTableData";
 import statisticsChartsData from "../../data/chartData";
 import ordersOverviewData from "../../data/ordersOverviewData";
 import { useSelector } from "react-redux";
-import {
-  BanknotesIcon,
-  UserPlusIcon,
-  UsersIcon,
-  ChartBarIcon,
-} from "@heroicons/react/24/solid";
+import AdminStatisticsCard from "../../components/features/user/statisticsCard/AdminStatisticsCard";
 
 export function Home() {
   const {user}=useSelector(state=>state.user)
-  const {PendingUserList}=useSelector(state=>state.user)
-
-  const adminStatisticsCardsData = [
-    {
-      color: "gray",
-      icon: BanknotesIcon,
-      title: "Today's Money",
-      value: "$53k",
-      footer: {
-        color: "text-green-500",
-        value: "+55%",
-        label: "than last week",
-      },
-    },
-  
-    {
-      color: "gray",
-      icon: UserPlusIcon,
-      title: "APPROVE USERS",
-      value: "Pending list",
-      footer: {
-        color: "text-red-500",
-        value: "7",
-        label: "users waiting for Attention",
-      },
-    },
-    {
-      color: "gray",
-      icon: ChartBarIcon,
-      title: "Sales",
-      value: "$103,430",
-      footer: {
-        color: "text-green-500",
-        value: "+5%",
-        label: "than yesterday",
-      },
-    },
-    {
-      color: "gray",
-      icon: UsersIcon,
-      title: "STUDENTS",
-      value: "CRM",
-      footer: {
-        color: "text-green-500",
-        value: "+3%",
-        label: "usage than last month",
-      },
-    },
-  ];
 
   return (
     <div className="mt-12">
@@ -86,23 +32,7 @@ export function Home() {
         
       {
         user.role==="admin" &&
-
-        adminStatisticsCardsData.map(({ icon, title, footer, ...rest }) => (
-          <StatisticsCard
-            key={title}
-            {...rest}
-            title={title}
-            icon={React.createElement(icon, {
-              className: "w-6 h-6 text-white",
-            })}
-            footer={
-              <Typography className="font-normal text-blue-gray-600">
-                <strong className={footer.color}>{footer.value}</strong>
-                &nbsp;{footer.label}
-              </Typography>
-            }
-          />
-        ))
+        <AdminStatisticsCard/>
       }
 
 
