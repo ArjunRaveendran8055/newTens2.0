6290181754346b82a251b5477c51fa92318956e4
hrithin <hrithin2@gmail.com>
2024-03-04 14:27:50 +0530

    single user api

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,7 @@
 const { asyncWrapper } = require("../helpers/asyncWrapper");
 const { UserModel } = require("../models/UserModel");
+const mongoose = require('mongoose');
+const { AppError } = require("../AppError");
 
 //get the pending userList
 const pendingUserController = asyncWrapper(async (req, res, next) => {
@@ -12,4 +14,43 @@ const pendingUserController = asyncWrapper(async (req, res, next) => {
   }
 });
 
-module.exports = { pendingUserController };
+//get all users
+
+const allUserController = asyncWrapper(async (req, res, next) => {
+    const allData = await UserModel.find();
+  
+    if (allData.length < 1) {
+      res.status(200).json({ message: "no users!" });
+    } else {
+      res.status(200).json({ count: allData.length, data: allData });
+    }
+  });
+
+  //get user by id
+  const oneUserController = asyncWrapper(async (req, res, next) => {
+
+    const userId = req.params.id;
+    
+
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+        throw new AppError(400, "invalid Id!");
+      }else{
+        const userData = await UserModel.findById(userId);
+
+        if(!userData){
+         
+            throw new AppError(404, "no user found!");
+            
+        }else{
+            res.status(200).json({  data: userData });
+        }
+
+      }
+    
+    
+
+   
+
+  });
+
+module.exports = { pendingUserController,allUserController,oneUserController };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,11 @@
-const { pendingUserController } = require("../controllers/userController")
+const { pendingUserController, allUserController, oneUserController } = require("../controllers/userController")
 
 const userRoutes=require("express").Router()
 
 
 userRoutes.get("/getPendingUserList",pendingUserController)
+userRoutes.get("/getAllUserList",allUserController)
+userRoutes.get("/getOneUser/:id",oneUserController)
 
 
 module.exports={userRoutes}
