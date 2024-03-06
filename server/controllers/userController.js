f452bb9743150ede33244c25988c7b7e686818c7
hrithin <hrithin2@gmail.com>
2024-03-06 14:28:13 +0530

    delete user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,6 @@
 const { asyncWrapper } = require("../helpers/asyncWrapper");
 const { UserModel } = require("../models/UserModel");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { AppError } = require("../AppError");
 
 //get the pending userList
@@ -17,71 +17,78 @@ const pendingUserController = asyncWrapper(async (req, res, next) => {
 //get all users
 
 const allUserController = asyncWrapper(async (req, res, next) => {
-    const allData = await UserModel.find();
-  
-    if (allData.length < 1) {
-      res.status(200).json({ message: "no users!" });
+  const allData = await UserModel.find();
+
+  if (allData.length < 1) {
+    res.status(200).json({ message: "no users!" });
+  } else {
+    res.status(200).json({ count: allData.length, data: allData });
+  }
+});
+
+//get user by id
+const oneUserController = asyncWrapper(async (req, res, next) => {
+  const userId = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    throw new AppError(400, "invalid Id!");
+  } else {
+    const userData = await UserModel.findById(userId);
+
+    if (!userData) {
+      throw new AppError(404, "no user found!");
     } else {
-      res.status(200).json({ count: allData.length, data: allData });
+      res.status(200).json({ data: userData });
     }
-  });
+  }
+});
 
-  //get user by id
-  const oneUserController = asyncWrapper(async (req, res, next) => {
+//get user by id
+const approveUserController = asyncWrapper(async (req, res, next) => {
+  const userId = req.params.id;
+  const { role } = req.body;
 
-    const userId = req.params.id;
-    
+  if (!role) {
+    throw new AppError(400, "role not specified");
+  }
 
-    if (!mongoose.Types.ObjectId.isValid(userId)) {
-        throw new AppError(400, "invalid Id!");
-      }else{
-        const userData = await UserModel.findById(userId);
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    throw new AppError(400, "invalid Id!");
+  } else {
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      userId,
+      { role, activestatus: true },
+      { new: true }
+    );
 
-        if(!userData){
-         
-            throw new AppError(404, "no user found!");
-            
-        }else{
-            res.status(200).json({  data: userData });
-        }
+    if (!updatedUser) {
+      throw new AppError(404, "no user found by id!");
+    } else {
+      res.status(200).json({ message: "user approved sucessfully!" });
+    }
+  }
+});
 
-      }
-    
-  });
+const deleteUserByIdController = asyncWrapper(async (req, res, next) => {
+  const userId = req.params.id;
 
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    throw new AppError(400, "Invalid ID!");
+  } else {
+    const deletedUser = await UserModel.findByIdAndDelete(userId);
 
+    if (!deletedUser) {
+      throw new AppError(404, "No user found by ID!");
+    } else {
+      res.status(200).json({ message: "User deleted successfully!" });
+    }
+  }
+});
 
-
-    //get user by id
-    const approveUserController = asyncWrapper(async (req, res, next) => {
-
-      const userId = req.params.id;
-      const { role } = req.body;
-      
-      if(!role){
-        throw new AppError(400,"role not specified")
-      }
-  
-      if (!mongoose.Types.ObjectId.isValid(userId)) {
-          throw new AppError(400, "invalid Id!");
-        }else{
-        
-      const updatedUser = await UserModel.findByIdAndUpdate(userId, { role,activestatus:true }, { new: true });
-  
-          if(!updatedUser ){
-           
-              throw new AppError(404, "no user found by id!");
-              
-          }else{
-              
-              res.status(200).json({  message: "user update sucessfully!" });
-          }
-  
-        }
-      
-    });
-  
-
-   
-
-module.exports = { pendingUserController,allUserController,oneUserController,approveUserController };
+module.exports = {
+  pendingUserController,
+  allUserController,
+  oneUserController,
+  approveUserController,
+  deleteUserByIdController,
+};
--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -3,7 +3,7 @@ const { AppError } = require("../AppError")
 const erroHandler=(error,req,res,next)=>{
     
     if(error instanceof AppError){
-        return res.status(error.statusCode).json({error:error.message})
+        return res.status(error.statusCode).json({success:false,error:error.message})
     }
     console.log(error.message);
     return res.status(500).json({error:"Internal Server Error"})
