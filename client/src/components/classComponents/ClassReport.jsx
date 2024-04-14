e958433e90782628f5000d16939f056f1241a71f
Fayaz900 <fayazazeez2@gmail.com>
2024-04-14 13:57:24 +0530

    report feched from database and shown in report page done

--- client/src/components/classComponents/ClassReport.jsx
+++ client/src/components/classComponents/ClassReport.jsx
@@ -22,9 +22,10 @@ function ClassReport() {
       reportedBy: "",
       followUp: false,
   });
-
-      // STATE FOR SAVE BUTTON ENABLE/DISABLE
-      const [isDataFetched, setIsDataFetched] = useState(false);
+    // state to show message after submitting report
+    const[saveMessage,setSaveMessage]=useState("")
+    // STATE FOR SAVE BUTTON ENABLE/DISABLE
+    const [isDataFetched, setIsDataFetched] = useState(false);
 
 
    useEffect(()=>{
@@ -38,10 +39,29 @@ function ClassReport() {
    const fetchData = async () => {
     if (searchRoll.length === 5) {
         try {
+            const responseReport = await axios.get(`http://localhost:8055/classReport/GetAllReport/${id}?roll=${searchRoll}`);
             const response = await axios.get(`http://localhost:8055/student/getAllStudents?roll=${searchRoll}`);
+            const fetchedReportData = responseReport.data.report; 
+            console.log(fetchedReportData)
             const fetchedStudentName = response.data.data[0].student_name;
             console.log(response.data.data[0]._id)
-            setStudentId(response.data.data[0]._id)
+            
+            if(fetchedReportData){
+              setStudentId(fetchedReportData._id);
+              setStudentName(fetchedStudentName);
+              setReportData(prevState => ({
+                ...prevState,
+                name: fetchedStudentName,
+                roll: searchRoll,
+                studentId: fetchedReportData._id,
+                report: fetchedReportData.report,
+                remark: fetchedReportData.remark,
+                reportedBy: user.firstname,
+                followUp: fetchedReportData.followUp,
+              }));
+              setIsDataFetched(true); 
+            }else{
+               setStudentId(response.data.data[0]._id)
             setStudentName(fetchedStudentName);
             setReportData(prevState => ({
                 ...prevState,
@@ -50,7 +70,10 @@ function ClassReport() {
                 reportedBy:user.firstname,
             }));
             setIsDataFetched(true); // Enable the save button
-        } catch (error) {
+        }
+       
+            }
+            catch (error) {
             console.log(error.response);
             setIsDataFetched(false); // Disable the save button
         }
@@ -109,15 +132,22 @@ function ClassReport() {
 
 // POST REQUEST TO SERVER
    const handleSave = async() => {
-    console.log(reportData)
    await axios.post(`http://localhost:8055/classReport/CreateReport/${id}`,{
     ...reportData,
     studentId,
   })
     .then((res)=>{
       console.log(res)
+      setSaveMessage("Report saved successfully!");
     })
-    .catch((err)=>console.log(err))
+    .catch((err)=>{
+      console.log(err)
+      setSaveMessage("Failed to save Report. Please try again.");
+    })
+
+    setTimeout(() => {
+      setSaveMessage("");
+    }, 1000);
 };
 
     
@@ -150,43 +180,43 @@ function ClassReport() {
 
         <div className="grid grid-cols-3 sm:grid-cols-1 lg:grid-cols-3 text-black gap-7 mb-6">
           <div className="flex items-center  space-x-2">
-            <input id="audioDisconnected" type="checkbox" onChange={handleCheckboxChange} className="rounded border-gray-300" />
+            <input id="audioDisconnected" type="checkbox" checked={reportData.report.includes("audioDisconnected")} onChange={handleCheckboxChange} className="rounded border-gray-300" />
             <label className="text-lg font-medium leading-none" htmlFor="audioDisconnected">
               Audio Disconnected
             </label>
           </div>
           <div className="flex items-center space-x-2">
-            <input id="videoDisconnected" type="checkbox" onChange={handleCheckboxChange} className="rounded border-gray-300" />
+            <input id="videoDisconnected" type="checkbox" checked={reportData.report.includes("videoDisconnected")} onChange={handleCheckboxChange} className="rounded border-gray-300" />
             <label className="text-lg font-medium leading-none" htmlFor="videoDisconnected">
               Video Disconnected
             </label>
           </div>
           <div className="flex items-center space-x-2">
-            <input id="rangeIssue" type="checkbox" onChange={handleCheckboxChange}  className="rounded border-gray-300" />
+            <input id="rangeIssue" type="checkbox" checked={reportData.report.includes("rangeIssue")} onChange={handleCheckboxChange}  className="rounded border-gray-300" />
             <label className="text-lg font-medium leading-none" htmlFor="rangeIssue">
               Range issue
             </label>
           </div>
           <div className="flex items-center space-x-2">
-            <input id="phoneUse" type="checkbox" onChange={handleCheckboxChange} className="rounded border-gray-300" />
+            <input id="phoneUse" type="checkbox" checked={reportData.report.includes("phoneUse")} onChange={handleCheckboxChange} className="rounded border-gray-300" />
             <label className="text-lg font-medium leading-none" htmlFor="phoneUse">
               Phone Use
             </label>
           </div>
           <div className="flex items-center space-x-2">
-            <input id="cameraArrangement" type="checkbox" onChange={handleCheckboxChange} className="rounded border-gray-300" />
+            <input id="cameraArrangement" type="checkbox" checked={reportData.report.includes("cameraArrangement")} onChange={handleCheckboxChange} className="rounded border-gray-300" />
             <label className="text-lg font-medium leading-none" htmlFor="cameraArrangement">
               Camera arrangement
             </label>
           </div>
           <div className="flex items-center space-x-2">
-            <input id="notAttending" type="checkbox" onChange={handleCheckboxChange} className="rounded border-gray-300" />
+            <input id="notAttending" type="checkbox" checked={reportData.report.includes("notAttending")} onChange={handleCheckboxChange} className="rounded border-gray-300" />
             <label className="text-lg font-medium leading-none" htmlFor="notAttending">
               Not attending
             </label>
           </div>
           <div className="flex items-center space-x-2">
-            <input id="leftWOTPermission" type="checkbox" onChange={handleCheckboxChange} className="rounded border-gray-300" />
+            <input id="leftWOTPermission" type="checkbox" checked={reportData.report.includes("leftWOTPermission")} onChange={handleCheckboxChange} className="rounded border-gray-300" />
             <label className="text-lg font-medium leading-none" htmlFor="leftWOTPermission">
               Left WOT permission
             </label>
@@ -201,25 +231,25 @@ function ClassReport() {
         <h2 className="text-xl text-black font-semibold mb-4">Exam Report</h2>
         <div className="grid text-black grid-cols-3 sm:grid-cols-1 lg:grid-cols-3 gap-7 mb-6">
           <div className="flex items-center space-x-2">
-            <input id="noParent" type="checkbox" onChange={handleCheckboxChange} className="rounded border-gray-300" />
+            <input id="noParent" type="checkbox" checked={reportData.report.includes("noParent")} onChange={handleCheckboxChange} className="rounded border-gray-300" />
             <label className="text-lg font-medium leading-none" htmlFor="noParent">
               No Parent
             </label>
           </div>
           <div className="flex items-center space-x-2">
-            <input id="copy" type="checkbox" onChange={handleCheckboxChange} className="rounded border-gray-300" />
+            <input id="copy" type="checkbox" checked={reportData.report.includes("copy")} onChange={handleCheckboxChange} className="rounded border-gray-300" />
             <label className="text-lg font-medium leading-none" htmlFor="copy">
               Copy
             </label>
           </div>
           <div className="flex items-center space-x-2">
-            <input id="copyDoubt" type="checkbox" onChange={handleCheckboxChange} className="rounded border-gray-300" />
+            <input id="copyDoubt" type="checkbox" checked={reportData.report.includes("copyDoubt")} onChange={handleCheckboxChange} className="rounded border-gray-300" />
             <label className="text-lg font-medium leading-none" htmlFor="copyDoubt">
               Copy Doubt
             </label>
           </div>
           <div className="flex items-center space-x-2">
-            <input id="parentNotVisible" type="checkbox" onChange={handleCheckboxChange} className="rounded border-gray-300" />
+            <input id="parentNotVisible" type="checkbox" checked={reportData.report.includes("parentNotVisible")} onChange={handleCheckboxChange} className="rounded border-gray-300" />
             <label className="text-lg font-medium leading-none" htmlFor="parentNotVisible">
               Parent Not Visible
             </label>
@@ -235,6 +265,7 @@ function ClassReport() {
             className="block w-full border rounded-md py-2 px-3 text-lg text-gray-700"
             id="remarks"
             placeholder="Type your remarks here."
+            value={reportData.remark}
             onChange={handleRemarkChange}
           ></textarea>
         </div>
@@ -242,6 +273,7 @@ function ClassReport() {
             <button onClick={handleSave} className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
                 Save
            </button>
+           {saveMessage && <p className={saveMessage.includes("successfully") ? "text-green-600 mt-4" : "text-red-600"}>{saveMessage}</p>}
                
       </div> 
       )}
