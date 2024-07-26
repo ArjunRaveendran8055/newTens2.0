6ccd244fe7ac3e61a64311da0a60af88db698c4f
unknown <fayazazeez2@gmail.com>
2024-07-26 12:45:10 +0530

    adding mentor and listing and deleting succesfully done

--- client/src/components/CentreandBatches/DisplayBatches.jsx
+++ client/src/components/CentreandBatches/DisplayBatches.jsx
@@ -340,29 +340,28 @@ function DisplayBatches() {
               <td className="px-4 py-3 text-gray-800 dark:text-gray-200">
                 <div className="relative h-10 w-32 min-w-[100px]">
 
-                  {batches[0].mentors && (
+                  {batch.mentors && (
                     <select
                       className="peer h-full w-32 rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 empty:!bg-gray-900 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                       onChange={handleSelectChange}
                     >
-                      <option value="">{batches[0].mentors[0].name}</option>
+                      <option value="">{batch.mentors[0]?.name || 'Select a Mentor'}</option>
                       {mentroNames
-                        .filter(mentor => mentor.id !== batches[0].mentors[0].id)
+                        .filter(mentor => batch.mentors[0]?.id !== mentor.id)
                         .map((mentor) => (
                           <option key={mentor.id} value={mentor.id}>
                             {mentor.name}
                           </option>
                         ))}
-                        {
-                          batches[0].mentors[0].name && (
-                            <option value="none">None</option>
-                          )
-                        }
-                        
+                      {
+                        batch.mentors[0]?.name && (
+                          <option value="none">None</option>
+                        )
+                      }
                     </select>
                   )}
 
-                  {!batches[0].mentors && (
+                  {!batch.mentors && (
                     <select
                       className="peer h-full w-32 rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 empty:!bg-gray-900 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                       onChange={handleSelectChange}
@@ -377,7 +376,6 @@ function DisplayBatches() {
                         ))}
                     </select>
                   )}
-
 
                   <label
                     className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-32 select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
