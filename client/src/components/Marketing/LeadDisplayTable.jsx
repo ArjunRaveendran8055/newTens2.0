4a4cfa23a0e8a7b77a3f3e72ed0df8c362c1abed
ArjunRaveendran8055 <arjunravi1213@gmail.com>
2024-08-15 10:59:49 +0530

    progressing lead view

--- client/src/components/Marketing/LeadDisplayTable.jsx
+++ client/src/components/Marketing/LeadDisplayTable.jsx
@@ -10,8 +10,7 @@ import {
 } from "@material-tailwind/react";
 import { CiExport } from "react-icons/ci";
 import authorsTableData from "../../data/authorsTableData";
-
-
+import CheckBox from "@mui/icons-material/CheckBox";
 
 const LeadDisplayTable = ({
   leadList,
@@ -22,26 +21,31 @@ const LeadDisplayTable = ({
   setTotalPages,
   itemsPerPage,
   tHeadings,
-  setTHeadings
+  setTHeadings,
 }) => {
   return (
     <div className="mt-12 mb-8 flex flex-col gap-12">
       <Card>
         <CardHeader className="mb-2 p-2 shadow-lg">
-          <Typography color="white" className="w-full flex justify-between">
-            <span className="text-3xl text-gray-600 font-extrabold">
-              Lead Details
-            </span>
-            <Button className="flex gap-2 justify-center items-center bg-black">
-              <span>Export</span> <CiExport size={25} />
-            </Button>
+          <Typography color="white" className="w-full flex flex-col">
+            <div className="LeadDetailsandExportDiv w-full flex justify-between items-center">
+              <span className="text-3xl text-gray-600 font-extrabold">
+                Lead Details
+              </span>
+              <Button className="flex gap-2 justify-center items-center bg-black">
+                <span>Export</span> <CiExport size={25} />
+              </Button>
+            </div>
+            <div className="checkSelectionDiv text-gray-500">
+              <CheckBox color="red" className="text-red">Dragon</CheckBox>
+            </div>
           </Typography>
         </CardHeader>
         <CardBody className="overflow-x-scroll px-0 pt-0 pb-2">
           <table className="w-full min-w-[640px] table-auto">
             <thead>
               <tr>
-                {["author", "function", "status", "employed", ""].map((el) => (
+                {tHeadings.map((el) => (
                   <th
                     key={el}
                     className="border-b border-blue-gray-50 py-3 px-5 text-left"
@@ -57,72 +61,67 @@ const LeadDisplayTable = ({
               </tr>
             </thead>
             <tbody>
-              {authorsTableData.map(
-                ({ img, name, email, job, online, date }, key) => {
-                  const className = `py-3 px-5 ${
-                    key === authorsTableData.length - 1
-                      ? ""
-                      : "border-b border-blue-gray-50"
-                  }`;
+              {leadList.map((item, key) => {
+                const className = `py-3 px-5 ${
+                  key === leadList.length - 1
+                    ? ""
+                    : "border-b border-blue-gray-50"
+                }`;
 
-                  return (
-                    <tr key={name}>
-                      <td className={className}>
-                        <div className="flex items-center gap-4">
-                          <Avatar
-                            src={img}
-                            alt={name}
-                            size="sm"
-                            variant="rounded"
-                          />
-                          <div>
-                            <Typography
-                              variant="small"
-                              color="blue-gray"
-                              className="font-semibold"
-                            >
-                              {name}
-                            </Typography>
-                            <Typography className="text-xs font-normal text-blue-gray-500">
-                              {email}
-                            </Typography>
-                          </div>
+                return (
+                  <tr key={key} className="uppercase">
+                    <td className={className}>
+                      <div className="flex items-center gap-4">
+                        <div>
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-semibold"
+                          >
+                            {item.name}
+                          </Typography>
                         </div>
-                      </td>
-                      <td className={className}>
-                        <Typography className="text-xs font-semibold text-blue-gray-600">
-                          {job[0]}
-                        </Typography>
-                        <Typography className="text-xs font-normal text-blue-gray-500">
-                          {job[1]}
-                        </Typography>
-                      </td>
-                      <td className={className}>
-                        <Chip
-                          variant="gradient"
-                          color={online ? "green" : "blue-gray"}
-                          value={online ? "online" : "offline"}
-                          className="py-0.5 px-2 text-[11px] font-medium w-fit"
-                        />
-                      </td>
-                      <td className={className}>
-                        <Typography className="text-xs font-semibold text-blue-gray-600">
-                          {date}
-                        </Typography>
-                      </td>
-                      <td className={className}>
-                        <Typography
-                          as="a"
-                          href="#"
-                          className="text-xs font-semibold text-blue-gray-600"
-                        >
-                          Edit
-                        </Typography>
-                      </td>
-                    </tr>
-                  );
-                }
-              )}
+                      </div>
+                    </td>
+                    <td className={className}>
+                      <Typography className="text-xs font-semibold text-blue-gray-600">
+                        {item.class}
+                      </Typography>
+                    </td>
+                    <td className={className}>{item.division}</td>
+                    <td className={className}>
+                      <Typography className="text-xs font-semibold text-blue-gray-600">
+                        {item.phone}
+                      </Typography>
+                    </td>
+                    <td className={className}>
+                      <Typography className="text-xs font-semibold text-blue-gray-600">
+                        {item.whatsapp}
+                      </Typography>
+                    </td>
+                    <td className={className}>
+                      <Typography className="text-xs font-semibold text-blue-gray-600">
+                        {item.syllabus}
+                      </Typography>
+                    </td>
+                    <td className={className}>
+                      <Typography className="text-xs font-semibold text-blue-gray-600">
+                        {item.school}
+                      </Typography>
+                    </td>
+                    <td className={className}>
+                      <Typography className="text-xs font-semibold text-blue-gray-600">
+                        {item.location}
+                      </Typography>
+                    </td>
+                    <td className={className}>
+                      <Typography className="text-xs font-semibold text-blue-gray-600">
+                        {item.district}
+                      </Typography>
+                    </td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </CardBody>
