0a307c8bfde5998dc28f8af60b4bd79113c81983
hrithin <hrithin2@gmail.com>
2024-08-31 11:22:03 +0530

    user create and edit by admin backend added

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,4 +1,4 @@
-const { pendingUserController, allUserController, oneUserController, approveUserController, deleteUserByIdController,getAllAAController } = require("../controllers/userController")
+const { pendingUserController, allUserController, oneUserController, approveUserController, deleteUserByIdController,getAllAAController, createUserController, editUserController } = require("../controllers/userController")
 const { authorizeGenuinity } = require("../middleware/authorizeGenuinity")
 
 const userRoutes=require("express").Router()
@@ -8,6 +8,8 @@ userRoutes.get("/getPendingUserList",authorizeGenuinity,pendingUserController)
 userRoutes.get("/getAllUserList",authorizeGenuinity,allUserController)
 userRoutes.get("/getAllAA",authorizeGenuinity,getAllAAController)
 userRoutes.get("/getOneUser/:id",authorizeGenuinity,oneUserController)
+userRoutes.post("/CreateNewUser",authorizeGenuinity,createUserController)
+userRoutes.put("/EditUser/:id",authorizeGenuinity,editUserController)
 userRoutes.put("/approveUser/:id",authorizeGenuinity,approveUserController)
 userRoutes.delete("/deleteUser/:id",authorizeGenuinity,deleteUserByIdController)
 
