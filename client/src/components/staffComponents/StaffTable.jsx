a8e0e4ddf1f1aee09ca5cb2aa497a92789e72519
unknown <fayazazeez2@gmail.com>
2024-08-31 11:24:26 +0530

    manage users

--- client/src/components/staffComponents/StaffTable.jsx
+++ client/src/components/staffComponents/StaffTable.jsx
@@ -10,11 +10,11 @@ const AuthorsTable = ({ authors }) => {
         <table className="min-w-full divide-y divide-gray-200">
           <thead className="bg-gray-50">
             <tr>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Designition</th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">DOB</th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Actions</th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-black uppercase tracking-wider">Name</th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-black uppercase tracking-wider">Designition</th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-black uppercase tracking-wider">Status</th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-black uppercase tracking-wider">DOB</th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-black uppercase tracking-wider">Actions</th>
             </tr>
           </thead>
           <tbody className="bg-white divide-y divide-gray-200">
@@ -26,28 +26,27 @@ const AuthorsTable = ({ authors }) => {
                       <img className="h-10 w-10 rounded-full" src={author.image} alt={author.name} />
                     </div>
                     <div className="ml-4">
-                      <div className="text-sm font-medium text-gray-900">{author.name}</div>
+                      <div className="text-s font-medium text-gray-900">{author.name}</div>
                       <div className="text-sm text-black">Email: {author.email}</div>
                     </div>
                   </div>
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap">
-                  <div className="text-sm text-gray-900">{author.function.title}</div>
-                  <div className="text-sm text-gray-500">{author.function.department}</div>
+                  <div className="text-s text-gray-900">{author.function.title}</div>
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap">
                   <span
                     className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${
                       author.status === 'Online'
                         ? 'bg-green-100 text-green-800'
-                        : 'bg-gray-100 text-gray-800'
+                        : 'bg-red-100 text-gray-800'
                     }`}
                   >
                     {author.status}
                   </span>
                 </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{author.employed}</td>
-                <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium flex gap-3">
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-black">{author.employed}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium flex gap-3 mt-2">
                  <a href="#" className="text-indigo-600 hover:text-indigo-900">Delete</a>
                   <a href="#" className="text-indigo-600 hover:text-indigo-900">Edit</a>
                 </td>
