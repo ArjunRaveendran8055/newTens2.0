fb3bb0f93289b1447b8e506c7fec9bc255dd1366
ArjunRaveendran8055 <arjunravi1213@gmail.com>
2024-09-20 17:04:50 +0530

    emptyTag added

--- client/src/components/classComponents/Attendance.jsx
+++ client/src/components/classComponents/Attendance.jsx
@@ -1,50 +1,52 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
-import { styled } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
 const Attendance = () => {
+  
   const { id } = useParams();
   return (
-    <div className="bg-white mt-5 h-[88vh] rounded-lg">
-      <div></div>
-      <FileUpload />
+    <div className="bg-white mt-5 h-[88vh] rounded-lg flex flex-col">
+      <div className="fileuploadContainer w-full flex flex-row gap-10 justify-center items-center pt-10">
+        <span className="font-bold text-xl">Click Here to Upload File</span>
+        <FileUpload />
+      </div>
     </div>
   );
 };
 
 export function FileUpload() {
-  
-  const VisuallyHiddenInput = styled('input')({
-    clip: 'rect(0 0 0 0)',
-    clipPath: 'inset(50%)',
+  const VisuallyHiddenInput = styled("input")({
+    clip: "rect(0 0 0 0)",
+    clipPath: "inset(50%)",
     height: 1,
-    overflow: 'hidden',
-    position: 'absolute',
+    overflow: "hidden",
+    position: "absolute",
     bottom: 0,
     left: 0,
-    whiteSpace: 'nowrap',
+    whiteSpace: "nowrap",
     width: 1,
   });
 
-
-
   return (
-    <Button
-      component="label"
-      role={undefined}
-      variant="contained"
-      tabIndex={-1}
-      startIcon={<CloudUploadIcon />}
-    >
-      Upload files
-      <VisuallyHiddenInput
-        type="file"
-        onChange={(event) => console.log(event.target.files)}
-        multiple
-      />
-    </Button>
+    <>
+      <Button
+        component="label"
+        role={undefined}
+        variant="contained"
+        tabIndex={-1}
+        startIcon={<CloudUploadIcon />}
+      >
+        Upload files
+        <VisuallyHiddenInput
+          type="file"
+          onChange={(event) => console.log(event.target.files)}
+          multiple
+        />
+      </Button>
+    </>
   );
 }
 
