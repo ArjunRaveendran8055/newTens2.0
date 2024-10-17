df7be412a579824bf81a37dbe12d924206419027
hrithin <hrithin2@gmail.com>
2024-10-17 17:02:53 +0530

    for some special changes

--- server/routes/classReportRoutes.js
+++ server/routes/classReportRoutes.js
@@ -1,11 +1,11 @@
-const { createReportController, getReportController, getClassStudentDetailsController, addMentorResponseController } = require("../controllers/classReportControllers");
+const { createReportController, getReportController, getClassStudentDetailsController, addMentorResponseController, getReportForClassController } = require("../controllers/classReportControllers");
 const { authorizeGenuinity } = require("../middleware/authorizeGenuinity");
 
 const classReportRoutes=require("express").Router()
 
 
 classReportRoutes.post('/CreateReport/:id',authorizeGenuinity(),createReportController)
-classReportRoutes.get('/GetAllReport/:id',authorizeGenuinity(),getReportController)
+classReportRoutes.get('/GetAllReport/:id',authorizeGenuinity(),getReportForClassController)
 classReportRoutes.get("/GetClassStudentDetails/:id",authorizeGenuinity(), getClassStudentDetailsController );
 classReportRoutes.post("/addMentorResponse",authorizeGenuinity(),addMentorResponseController)
 
